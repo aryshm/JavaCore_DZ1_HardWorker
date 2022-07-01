4c51a9e6cc59175c9c8e20b0d29304b1333cc128
aryshm <arishensky@yandex.ru>
2022-07-01 14:08:53 +0400

    Comments deleted

--- src/Worker.java
+++ src/Worker.java
@@ -3,10 +3,6 @@ public class Worker {
     private OnTaskDoneListener callback;
     private OnTaskErrorListener errorCallback;
 
-//    public Worker(OnTaskDoneListener callback) {
-//        this.callback = callback;
-//    }
-
     public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
         this.callback = callback;
         this.errorCallback = errorCallback;
