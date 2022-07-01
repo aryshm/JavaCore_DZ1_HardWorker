b94bbd087c3512c1ad2a4d392b649669babcdb11
aryshm <arishensky@yandex.ru>
2022-07-01 14:07:15 +0400

    Initial commit

---
+++ src/Worker.java
@@ -0,0 +1,25 @@
+public class Worker {
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+//    public Worker(OnTaskDoneListener callback) {
+//        this.callback = callback;
+//    }
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " is not done. Has an error");
+            } else {
+                callback.onDone("Task " + i + " is done");
+            }
+        }
+    }
+
+}
